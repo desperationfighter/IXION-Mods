32edda5825360aae7fdc97a0852fcfef697032b6
desperationfighter <patrick_as_desp@yahoo.de>
2022-12-12 00:28:14 +0100

    Add project files.

---
+++ VohleJumpHullDamageReduce/Patch/TorusStructure_CommandHullConsumption.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using BulwarkStudios.Stanford.Torus.Structure;
+using BulwarkStudios.Stanford.Core.GameStates;
+using BepInEx.Logging;
+
+namespace VohleJumpHullDamageReduce.Patch
+{
+    [HarmonyPatch(typeof(CommandHullConsumption))]
+    [HarmonyPatch(nameof(CommandHullConsumption.HandleVohleJump))]
+    internal static class TorusStructure_CommandHullConsumption
+    {
+        [HarmonyPrefix]
+        static bool Prefix(CommandHullConsumption __instace)
+        {
+            Game.Torus.torus.State.hullIntegrity -= 50f;
+            Plugin.logger.LogInfo("Set custom hull damange on Jump hopefully");
+            return false;
+        }
+    }
+}
---
+++ VohleJumpHullDamageReduce/Plugin.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.IL2CPP;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+
+namespace VohleJumpHullDamageReduce
+{
+    [BepInPlugin("Desperationfighter.IXIONMods.VohleJumpHullDamageReduce", "Vohle Jump Hull Damage Reduce", "1.0.0")]
+    public class Plugin : BasePlugin
+    {
+        public static ConfigEntry<int> ReduceDamageperJump;
+        public static ManualLogSource logger;
+
+        public override void Load()
+        {
+            ReduceDamageperJump = Config.Bind("General", "ReduceDamageperJump", 2, "Value how much Damage will be applied on each Jump");
+            
+            var harmony = new Harmony("Desperationfighter.IXIONMods.VohleJumpHullDamageReduce");
+            harmony.PatchAll();
+        }
+    }
+}
